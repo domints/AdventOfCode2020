ea610bda5445a48778f0518b1575ca5eb77e1374
Dominik Szymański <domints@gmail.com>
2020-12-09 14:15:44 +0100

    Add solution for Day 7

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day06
 {
@@ -6,7 +8,74 @@ namespace Day06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rows = System.IO.File.ReadAllLines("input.txt");
+            var groups = CountYesAnswersInGroups(rows);
+            Console.WriteLine($"Part 1: {groups.Sum()}");
+            var commonGroups = CountCommonYesAnswersInGroups(rows);
+            Console.WriteLine($"Part 2: {commonGroups.Sum()}");
+        }
+
+        static List<int> CountCommonYesAnswersInGroups(IEnumerable<string> rows)
+        {
+            List<int> result = new List<int>();
+
+            HashSet<char> currentGroup = new HashSet<char>();
+            var firstInGroup = true;
+            foreach (var r in rows)
+            {
+                if (string.IsNullOrWhiteSpace(r))
+                {
+                    result.Add(currentGroup.Count);
+                    currentGroup = new HashSet<char>();
+                    firstInGroup = true;
+                    continue;
+                }
+
+                if (firstInGroup)
+                {
+                    foreach (var ch in r)
+                    {
+                        if (!currentGroup.Contains(ch))
+                            currentGroup.Add(ch);
+                    }
+                }
+                else 
+                {
+                    foreach(var ch in currentGroup)
+                    {
+                        if(!r.Contains(ch))
+                            currentGroup.Remove(ch);
+                    }
+                }
+                
+                firstInGroup = false;
+            }
+
+            return result;
+        }
+
+        static List<int> CountYesAnswersInGroups(IEnumerable<string> rows)
+        {
+            List<int> result = new List<int>();
+
+            HashSet<char> currentGroup = new HashSet<char>();
+            foreach (var r in rows)
+            {
+                if (string.IsNullOrWhiteSpace(r))
+                {
+                    result.Add(currentGroup.Count);
+                    currentGroup = new HashSet<char>();
+                    continue;
+                }
+
+                foreach (var ch in r)
+                {
+                    if (!currentGroup.Contains(ch))
+                        currentGroup.Add(ch);
+                }
+            }
+
+            return result;
         }
     }
 }
--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day07
 {
@@ -6,7 +8,97 @@ namespace Day07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rows = System.IO.File.ReadAllLines("input.txt");
+            var bags = GetBags(rows);
+            MakeConnections(bags);
+            var myBag = bags["shiny gold"];
+            var possibleOutermostBags = new HashSet<string>();
+            FindPossibleOutermostBags(myBag, possibleOutermostBags);
+            Console.WriteLine($"Part 1: {possibleOutermostBags.Count}");
+
+            var innerBagsCount = CountInnerBags(myBag);
+            Console.WriteLine($"Part 2: {innerBagsCount}");
         }
+
+        static Dictionary<string, Bag> GetBags(IEnumerable<string> rows)
+        {
+            Dictionary<string, Bag> result = new Dictionary<string, Bag>();
+
+            foreach (var row in rows)
+            {
+                var bag = new Bag();
+                var definition = row.Split("contain").Select(d => d.Trim()).ToArray();
+                bag.Name = string.Join(' ', definition[0].Split(' ').Take(2));
+                bag.ContainementDefinitions = definition[1].Split(',').Select(d => d.Trim().Trim('.')).ToList();
+                result.Add(bag.Name, bag);
+            }
+
+            return result;
+        }
+
+        static void MakeConnections(Dictionary<string, Bag> bags)
+        {
+            foreach (var bag in bags.Values)
+            {
+                foreach (var def in bag.ContainementDefinitions)
+                {
+                    try
+                    {
+                        var splitDef = def.Split(' ');
+                        if (splitDef[0] == "no")
+                            continue;
+
+                        var name = $"{splitDef[1]} {splitDef[2]}";
+                        var count = int.Parse(splitDef[0]);
+                        var containedBag = bags[name];
+                        containedBag.ContainedIn.Add(bag);
+                        bag.Contains.Add((count, containedBag));
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"EXC! {def}");
+                        throw;
+                    }
+                }
+            }
+        }
+
+        static void FindPossibleOutermostBags(Bag currentBag, HashSet<string> possibilities)
+        {
+            foreach (var bag in currentBag.ContainedIn)
+            {
+                if (!possibilities.Contains(bag.Name))
+                {
+                    possibilities.Add(bag.Name);
+                    FindPossibleOutermostBags(bag, possibilities);
+                }
+            }
+        }
+
+        static int CountInnerBags(Bag currentBag)
+        {
+            var count = 0;
+            foreach (var bag in currentBag.Contains)
+            {
+                count += bag.count + (bag.count * CountInnerBags(bag.bag));
+            }
+
+            return count;
+        }
+    }
+
+    class Bag
+    {
+        public Bag()
+        {
+            ContainedIn = new List<Bag>();
+            ContainementDefinitions = new List<string>();
+            Contains = new List<(int count, Bag bag)>();
+        }
+
+        public string Name { get; set; }
+        public List<Bag> ContainedIn { get; set; }
+        public List<string> ContainementDefinitions { get; set; }
+        public List<(int count, Bag bag)> Contains { get; set; }
     }
 }
