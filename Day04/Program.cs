c5d73526b0a206d056020974a46424b4b4f10bd6
Dominik Szymański <domints@gmail.com>
2020-12-09 12:03:44 +0100

    Add Day 9 solution

---
+++ Day04/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Day04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = System.IO.File.ReadAllLines("input.txt");
+            var passports = ParsePassports(rows);
+            Console.WriteLine($"Part 1: {passports.Count(p => p.ValidSimple)}");
+            Console.WriteLine($"Part 2: {passports.Count(p => p.Valid)}");
+        }
+
+        static List<Passport> ParsePassports(IEnumerable<string> rows)
+        {
+            List<Passport> result = new List<Passport>();
+            var current = new Passport();
+            foreach (var row in rows)
+            {
+                if(string.IsNullOrWhiteSpace(row))
+                {
+                    result.Add(current);
+                    current = new Passport();
+                    continue;
+                }
+
+                var fields = row.Trim().Split(' ');
+                foreach (var field in fields)
+                {
+                    var f = field.Split(':');
+                    switch (f[0])
+                    {
+                        case "byr":
+                            current.BirthDate = f[1];
+                            break;
+                        case "iyr":
+                            current.IssueYear = f[1];
+                            break;
+                        case "eyr":
+                            current.ExpirationYear = f[1];
+                            break;
+                        case "hgt":
+                            current.Height = f[1];
+                            break;
+                        case "ecl":
+                            current.EyeColour = f[1];
+                            break;
+                        case "hcl":
+                            current.HairColour = f[1];
+                            break;
+                        case "pid":
+                            current.PassportId = f[1];
+                            break;
+                        case "cid":
+                            current.CountryId = f[1];
+                            break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class Passport
+    {
+        public string BirthDate { get; set; }
+        public string IssueYear { get; set; }
+        public string ExpirationYear { get; set; }
+        public string Height { get; set; }
+        public string EyeColour { get; set; }
+        public string HairColour { get; set; }
+        public string PassportId { get; set; }
+        public string CountryId { get; set; }
+
+        public bool ValidSimple => 
+            !string.IsNullOrWhiteSpace(BirthDate) &&
+            !string.IsNullOrWhiteSpace(IssueYear) &&
+            !string.IsNullOrWhiteSpace(ExpirationYear) &&
+            !string.IsNullOrWhiteSpace(Height) &&
+            !string.IsNullOrWhiteSpace(EyeColour) &&
+            !string.IsNullOrWhiteSpace(HairColour) &&
+            !string.IsNullOrWhiteSpace(PassportId);
+
+        public bool Valid => 
+            !string.IsNullOrWhiteSpace(BirthDate) && YearBetween(BirthDate, 1920, 2002) &&
+            !string.IsNullOrWhiteSpace(IssueYear) && YearBetween(IssueYear, 2010, 2020) &&
+            !string.IsNullOrWhiteSpace(ExpirationYear) && YearBetween(ExpirationYear, 2020, 2030) &&
+            !string.IsNullOrWhiteSpace(Height) && IsHeight(Height) &&
+            !string.IsNullOrWhiteSpace(EyeColour) && IsEyeColour(EyeColour) &&
+            !string.IsNullOrWhiteSpace(HairColour) && IsHexColour(HairColour) &&
+            !string.IsNullOrWhiteSpace(PassportId) && IsPassportId(PassportId);
+
+        private bool YearBetween(string value, int start, int end)
+        {
+            Regex rx = new Regex("^[0-9]{4}$");
+            return rx.IsMatch(value) && int.TryParse(value, out int o) && o >= start && o <= end;
+        }
+
+        private bool IsHeight(string value)
+        {
+            if(int.TryParse(value.Substring(0, value.Length - 2), out int hght))
+            {
+                if(new Regex("^[0-9]{1,20}cm").IsMatch(value) && hght >= 150 && hght <= 193)
+                    return true;
+                if(new Regex("^[0-9]{1,20}in").IsMatch(value) && hght >= 59 && hght <= 76)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private bool IsEyeColour(string value)
+        {
+            var allowed = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            return allowed.Contains(value);
+        }
+
+        private bool IsHexColour(string value)
+        {
+            return new Regex("^#[0-9a-f]{6}$").IsMatch(value);
+        }
+
+        private bool IsPassportId(string value)
+        {
+            return new Regex("^[0-9]{9}$").IsMatch(value);
+        }
+    }
+}
---
+++ Day09/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<long> preamble = new Queue<long>();
+
+            var numbers = System.IO.File.ReadAllLines("input.txt")
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .Select(l => long.Parse(l.Trim())).ToList();
+
+            for(int i = 0; i < 25; i++)
+            {
+                preamble.Enqueue(numbers[i]);
+            }
+
+            for(int i = 25; i < numbers.Count; i++)
+            {
+                if(!IsSumOfPrevious(numbers[i], preamble.ToList()))
+                {
+                    Console.WriteLine($"Part 1: {numbers[i]}");
+                    var block = FindContinousBlockLeadingTo(numbers[i], i, numbers);
+                    if(block.start == -1 || block.start == block.end)
+                        continue;
+                    
+                    Console.WriteLine($"Part 2: {SumOfMinMax(block, numbers)}");
+
+                    return;
+                }
+
+                preamble.Dequeue();
+                preamble.Enqueue(numbers[i]);
+            }
+        }
+
+        static long SumOfMinMax((int start, int end) block, List<long> numbers)
+        {
+            long min = long.MaxValue;
+            long max = 0;
+            for (int i = block.start; i <= block.end; i++)
+            {
+                if(numbers[i] < min)
+                    min = numbers[i];
+                if(numbers[i] > max)
+                    max = numbers[i];
+            }
+
+            return min + max;
+        }
+
+        static (int start, int end) FindContinousBlockLeadingTo(long number, int numberIx, List<long> numbers)
+        {
+            for(int i = 0; i < numberIx - 1; i++)
+            {
+                long currentSum = numbers[i];
+                for(int x = i + 1; x < numberIx; x++)
+                {
+                    currentSum += numbers[x];
+                    if(currentSum == number)
+                        return (i, x);
+
+                    if(currentSum > number)
+                        break;
+                }
+            }
+
+            return (-1, -1);
+        }
+
+        static bool IsSumOfPrevious(long number, List<long> previous)
+        {
+            for(int x = 0; x < previous.Count - 1; x++)
+            {
+                for (int y = 1; y < previous.Count; y++)
+                {
+                    if(previous[x] + previous[y] == number)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
